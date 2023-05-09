87c6abb6e9bdbc4baf0e049f067ce3eb0dcb9486
Bright Omolayo <Molayobright@gmail.com>
2023-05-09 16:33:59 +0100

    edited the side ar

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopNav");
-    if (x.className === "topNav") {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "topNav";
+      x.className = "topnav";
     }
   }
